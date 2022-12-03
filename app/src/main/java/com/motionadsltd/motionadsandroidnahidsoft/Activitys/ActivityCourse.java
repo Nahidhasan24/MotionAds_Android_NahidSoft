9344c94b7caf7fbefbb8bbd0639ce26e6ffe0f8e
Nahid <nahidhasan24>
2022-12-04 00:46:56 +0600

    msg

--- app/src/main/java/com/motionadsltd/motionadsandroidnahidsoft/Activitys/ActivityCourse.java
+++ app/src/main/java/com/motionadsltd/motionadsandroidnahidsoft/Activitys/ActivityCourse.java
@@ -1,19 +1,72 @@
 package com.motionadsltd.motionadsandroidnahidsoft.Activitys;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.admanager.AdManagerAdRequest;
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAd;
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAdLoadCallback;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.motionadsltd.motionadsandroidnahidsoft.R;
+import com.motionadsltd.motionadsandroidnahidsoft.databinding.ActivityCourseBinding;
 
 public class ActivityCourse extends AppCompatActivity {
 
 
+    ActivityCourseBinding binding;
+    private AdManagerInterstitialAd mAdManagerInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course);
+        binding=ActivityCourseBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+
+        loadAds();
 
 
+
+    }
+
+    private void loadAds() {
+        AdManagerAdRequest interads = new AdManagerAdRequest.Builder().build();
+        AdManagerInterstitialAd.load(this,getString(R.string.interads_admob), interads,
+                new AdManagerInterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull AdManagerInterstitialAd interstitialAd) {
+                        // The mAdManagerInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mAdManagerInterstitialAd = interstitialAd;
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        mAdManagerInterstitialAd = null;
+                    }
+                });
+        AdView adView = new AdView(this);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        binding.adView.loadAd(adRequest);
+        binding.adView1.loadAd(adRequest);
+        binding.adView2.loadAd(adRequest);
+        if (mAdManagerInterstitialAd != null) {
+            mAdManagerInterstitialAd.show(ActivityCourse.this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
     }
 }
